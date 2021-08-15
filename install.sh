314b5dc3b4f553b91bc64d11b274dfdc3c0c5a7b
Ilder Velez <velez.ilder@gmail.com>
2021-08-15 19:39:10 -0400

    update

--- install.sh
+++ install.sh
@@ -191,6 +191,12 @@ sudo apt-get update
 sudo apt-get install papirus-icon-theme
 echo "Successfully installed papirus icons"
 
+# install vscodium
+echo "Installing vscodium..."
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg 
+echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
+sudo apt update && sudo apt install codium -y
+echo "Successfully installed vscodium"
 
 # TODO(ivel): Research how to install these programs automatically
 # install vscode
