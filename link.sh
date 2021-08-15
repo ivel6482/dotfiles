54dc7337ff6c6d53d7cfab33a848cab6f4aeda48
Ilder Velez <velez.ilder@gmail.com>
2021-08-15 00:24:51 -0400

    update

--- link.sh
+++ link.sh
@@ -3,4 +3,8 @@
 echo "Linking dotfiles..."
 ln -sf ~/Documents/dotfiles/.config/nvim ~/.config/nvim
 ln -sf ~/Documents/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
+ln -sf ~/Documents/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/Documents/dotfiles/tmux ~/tmux
+ln -sf ~/Documents/dotfiles/.config/i3 ~/.config/i3
+ln -sf ~/Documents/dotfiles/.config/polybar ~/.config/polybar
 echo "Success"
