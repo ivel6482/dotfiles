fef7ec9d149b7669e081f5de1d1c21576bcaed22
Ilder Velez <velez.ilder@gmail.com>
2024-10-23 22:47:42 -0400

    todo

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 # TODO: automate installation of packages
 # If a package in the list is already installed on the system, it will be reinstalled even if it is already up to date. This behavior can be overridden with the --needed option.
+#
+# TODO: Add nvm
 
 sudo pacman -S --needed \
  git                    \
