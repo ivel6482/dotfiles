6b7b69247c7b9cb0338707e7a47bc332ddba7b9f
Ilder Velez <velez.ilder@gmail.com>
2024-10-25 15:06:43 -0400

    show hidden files

--- nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -15,6 +15,10 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_hidden = false,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
