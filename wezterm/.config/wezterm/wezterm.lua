214ee5a0d015daa05f9e7afb9487204c04782c51
Ilder Velez <velez.ilder@gmail.com>
2024-10-25 12:16:44 -0400

    toggle opacity

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,18 +2,41 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config = {
-	automatically_reload_config = true,
-	enable_tab_bar = false,
-	-- window_decorations = "RESIZE",
-	font = wezterm.font("Terminess Nerd Font Mono", { weight = "Regular", italic = false }),
-	font_size = 20,
-	color_scheme = "tokyonight",
-	window_padding = {
-		top = 0,
-		right = 0,
-		bottom = 0,
-		left = 0,
+config.automatically_reload_config = true
+
+config.enable_tab_bar = false
+
+-- config.window_decorations = "RESIZE"
+
+config.font = wezterm.font("Terminess Nerd Font Mono", { weight = "Regular", italic = false })
+config.font_size = 20
+
+config.color_scheme = "tokyonight"
+
+config.window_background_opacity = 0.9
+
+config.window_padding = {
+	top = 0,
+	right = 0,
+	bottom = 0,
+	left = 0,
+}
+
+config.keys = {
+	{
+		key = "O",
+		mods = "CTRL|ALT",
+		action = wezterm.action_callback(function(window, _)
+			local overrides = window:get_config_overrides() or {}
+
+			if overrides.window_background_opacity == 1.0 then
+				overrides.window_background_opacity = 0.9
+			else
+				overrides.window_background_opacity = 1.0
+			end
+
+			window:set_config_overrides(overrides)
+		end),
 	},
 }
 
