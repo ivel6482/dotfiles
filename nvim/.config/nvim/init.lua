09698819bd2e348a21c8c874bc2d7103e173d770
Ilder Velez <velez.ilder@gmail.com>
2024-10-25 12:03:13 -0400

    transparency

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -832,6 +832,9 @@ require('lazy').setup({
       { '<C-l>', ':TmuxNavigateRight<CR>' },
     },
   },
+  {
+    'xiyaowong/transparent.nvim',
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
